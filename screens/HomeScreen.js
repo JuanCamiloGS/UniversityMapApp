f17fe038baaace5a56afe1f96cf4af0418890ef1
Juan Camilo Gomez Santofimio <juan.camilo.gomez95@gmail.com>
2019-05-12 21:50:17 -0500

    origin point for routing

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -36,6 +36,7 @@ class Home extends Component<{}> {
     FloorButton = this.FloorButton.bind(this);
     getRouteData = this.getRouteData.bind(this);
     selectedLocation = this.selectedLocation.bind(this);
+    selectedOrigin = this.selectedOrigin.bind(this);
     RouteDescription = this.RouteDescription.bind(this);
     dataFetcher = this.dataFetcher.bind(this);
 
@@ -53,12 +54,14 @@ class Home extends Component<{}> {
       isPermissionGranted: false,
       hidden: true,
       hidden2: false,
+      hidden3: true,
       pointCurrent: {
         "type": "Point",
         "coordinates": [0,0],
         "hidden": true,
         "properties": {}
       },
+      pointOrig: {"properties": {"name":"Tu posición"}},
       floorMap: Floors[1].map,
       currentFloor: 1,
       route: {
@@ -67,7 +70,8 @@ class Home extends Component<{}> {
         "hidden": true
       },
       legs: [],
-      query: ''
+      query: '',
+      oriquer: ''
     }
 
     this.onSourceLayerPress = this.onSourceLayerPress.bind(this);
@@ -109,6 +113,7 @@ class Home extends Component<{}> {
   showMenu(obj){
     this.setState({
       hidden: false,
+      hidden3: false,
       pointCurrent: obj
     })
   }
@@ -122,12 +127,15 @@ class Home extends Component<{}> {
   closeVis(){
     this.setState({
       hidden: true,
+      hidden3: true,
       pointCurrent: {
         "type": "Point",
         "coordinates": [0,0],
         "hidden": true,
         "properties": {}
       },
+      pointOrig: {"properties": {"name":"Tu posición"}},
+      oriquer: '',
       route: {
         "type": "LineString",
         "coordinates": [],
@@ -138,11 +146,11 @@ class Home extends Component<{}> {
 
   }
 
-  findData(query) {
-    if (query === '') {
+  findData(serch) {
+    if (serch === '') {
       return [];
     }
-    const regex = new RegExp(`${query.trim()}`, 'i');
+    const regex = new RegExp(`${serch.trim()}`, 'i');
     return POI.filter(point => point['properties']['name'].search(regex) >= 0);
   }
 
@@ -157,6 +165,20 @@ class Home extends Component<{}> {
       legs: []
     }); 
     showMenu(obj);
+    Keyboard.dismiss();
+  }
+
+  selectedOrigin(obj){
+    this.setState({ 
+      oriquer: obj['properties']['name'],
+      pointOrig: obj,
+      route: {
+        "type": "LineString",
+        "coordinates": [],
+        "hidden": true
+      },
+      legs: []
+    });
     Keyboard.dismiss();
   }
 
@@ -197,7 +219,7 @@ class Home extends Component<{}> {
             route: polyline.toGeoJSON(responseJson['trip']['legs'][0]['shape'],6),
             legs: responseJson['trip']['legs'][0]
           })
-          Alert.alert(JSON.stringify(responseJson));
+          //Alert.alert(JSON.stringify(responseJson));
         }else{
           Alert.alert("No se encontró ruta");
         }
@@ -210,6 +232,7 @@ class Home extends Component<{}> {
 
   getRouteData(){
     const obj = this.state.pointCurrent;
+
     var destiny = {};
     if(obj['properties']['indoor'] == 'no'){
       coords = obj['properties']['roadPoint'].split(',').map(Number);
@@ -217,30 +240,44 @@ class Home extends Component<{}> {
       destiny['longitude'] = coords[1];
     }else{
       outdoorPoint = Outdoors.filter(point => point['properties']['name'] == obj['properties']['isin'])[0];
-      //console.log(Outdoors);
       coords = outdoorPoint['properties']['roadPoint'].split(',').map(Number);
       destiny['latitude'] = coords[0];
       destiny['longitude'] = coords[1];
     }
-    if (this.state.isPermissionGranted) {
-      Geolocation.getCurrentPosition(
-          (position) => {
-              console.log(position);
-              dataFetcher(position, destiny);
-          },
-          (error) => {
-              // See error code charts below.
-              //console.log(error.code, error.message);
-              //Alert.alert("Error de localización: "+ error.message)
-              navigator.geolocation.getCurrentPosition(
-                (position) => {dataFetcher(position, destiny)}, 
-                (error) => {Alert.alert("No se pudo obtener localización")}, 
-                {enableHighAccuracy: true, timeout: 20000, maximumAge: 80000});
-          },
-          { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-      );
+
+    console.log("La respuesta es: "+!(this.state.pointOrig['type']=='Feature'))
+    if(!(this.state.pointOrig['type']=='Feature')){
+      if (this.state.isPermissionGranted) {
+        Geolocation.getCurrentPosition(
+            (position) => {
+                dataFetcher(position, destiny);
+            },
+            (error) => {
+                navigator.geolocation.getCurrentPosition(
+                  (position) => {dataFetcher(position, destiny)}, 
+                  (error) => {Alert.alert("No se pudo obtener localización")}, 
+                  {enableHighAccuracy: true, timeout: 20000, maximumAge: 80000});
+            },
+            { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+        );
+      }else{
+        Alert.alert("Se requieren permisos para acceder a la localización de usuario")
+      }
     }else{
-      Alert.alert("Se requieren permisos para acceder a la localización de usuario")
+      const init = this.state.pointOrig;
+      var origin = {'coords': {}};
+      if(init['properties']['indoor'] == 'no'){
+        coords = init['properties']['roadPoint'].split(',').map(Number);
+        origin['coords']['latitude'] = coords[0];
+        origin['coords']['longitude'] = coords[1];
+      }else{
+        outdoorPoint = Outdoors.filter(point => point['properties']['name'] == init['properties']['isin'])[0];
+        coords = outdoorPoint['properties']['roadPoint'].split(',').map(Number);
+        origin['coords']['latitude'] = coords[0];
+        origin['coords']['longitude'] = coords[1];
+      }
+
+      dataFetcher(origin, destiny);
     }
   }
 
@@ -326,8 +363,14 @@ class Home extends Component<{}> {
           <Text style={{fontSize: 12}}>Tiempo: {this.state.legs['summary']['time']} s</Text>
           <Text style={{fontSize: 12}}>Distancia: {this.state.legs['summary']['length']*1000} m</Text>
         </View>
-        <Text style={{fontWeight: 'bold',fontSize: 10}}>Indicaciones:</Text>
 
+        <Text style={{fontWeight: 'bold',fontSize: 10}}>Origen:</Text>
+        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', marginBottom: 4}}>
+          <Text style={{fontSize: 12}}>Ubicacion: {this.state.pointOrig['properties']['name'] || 'Error'}</Text>
+          <Text style={{fontSize: 12}}>Piso: {this.state.pointOrig['properties']['level'] || 'N/A'}</Text>
+        </View>
+        
+        <Text style={{fontWeight: 'bold',fontSize: 10}}>Indicaciones:</Text>
         <View key='I0' style={styles.routeStep}><Text style={{flex:8, fontSize: 12}}>Ubicate en el punto de partida de la ruta</Text><Text style={{flex:2, fontSize: 12}}> - </Text></View>
         {this.state.legs['maneuvers'].map((leg, i) => <View key={'I'+i} style={styles.routeStep}>
               <Text style={{flex:8, fontSize: 12}}>{leg['instruction']}</Text><Text style={{flex:2, fontSize: 12}}>{leg['length']*1000} m</Text>
@@ -344,10 +387,15 @@ class Home extends Component<{}> {
     const slot = this.props.slot;
     const annotations = OptionSelect(slot);
 
+    const comp = (a, b) => a.toLowerCase().trim() === b.toLowerCase().trim();
     const query = this.state.query;
     const data = this.findData(query);
-    const comp = (a, b) => a.toLowerCase().trim() === b.toLowerCase().trim();
     const rows = data.length === 1 && comp(query, data[0]['properties']['name']) ? [] : data.slice(0,5);
+
+    const oriquer = this.state.oriquer;
+    const data2 = this.findData(oriquer);
+    var rows2 = data2.length === 1 && comp(oriquer, data2[0]['properties']['name']) ? [] : data2.slice(0,4);
+    rows2 = rows2.length > 0? rows2.concat({'properties':{'name':'Tu posición'}}) : rows2;
 
     return (
       <View style={styles.container}>
@@ -355,7 +403,7 @@ class Home extends Component<{}> {
         {/* Map Object */}
         <MapboxGL.MapView styleURL={'asset://style/bright.json'}
                         zoomLevel={16.1} 
-                        centerCoordinate={[-74.8503,11.0191]} 
+                        centerCoordinate={[-74.8503,11.0191]}
                         style={styles.container}
                         minZoomLevel={16}
                         compassEnabled={false}
@@ -411,6 +459,24 @@ class Home extends Component<{}> {
                 </TouchableOpacity>
               )}
             />
+            {!this.state.hidden3 && 
+            <Autocomplete
+              data={rows2} 
+              defaultValue={oriquer}
+              onChangeText={text => this.setState({ oriquer: text })}
+              placeholder="Origen: Tu ubicación actual"
+              keyExtractor={(item, index) => ("S2"+index)}
+              containerStyle={{margin: 2}}
+              inputContainerStyle={{borderWidth: 0, borderBottomWidth: 1, marginBottom: 1}}
+              listStyle={{borderBottomWidth: 0}}
+              renderItem={({item, index} ) => (
+                <TouchableOpacity onPress={() => selectedOrigin(item)}>
+                  <Text style={styles.itemText}>
+                    {item['properties']['name']} (Piso: {item['properties']['level'] || 'N/A'})
+                  </Text>
+                </TouchableOpacity>
+              )}
+            />}
           </View>
         </View>
         }
